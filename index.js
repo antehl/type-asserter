ef7f77d0b01d05449641f523ebc4943b0542fddd
antehl <75947069+antehl@users.noreply.github.com>
2022-03-14 23:38:23 +0100

    Add isType and slightly better "type weirdness" explanation

--- index.js
+++ index.js
@@ -6,9 +6,7 @@ const valueFormat = value => {
 	if (type === "string") return `'${value}'`;
 	if (type === "bigint") return `${value}n`;
 	if (type === "symbol") return value.toString();
-
 	if (value instanceof RegExp) return value.toString();
-
 	return value.name;
 };
 
@@ -39,30 +37,28 @@ const sendError = (values, types, invert = false) => {
 	);
 };
 
-// 1 instanceof Number ............ -> false
-// typeof 1	....................... -> "number"
+// 1 instanceof Number  . . . . . .	-> false
+// new Number(1) instanceof Number 	-> true
+// Number instanceof Number . . . .	-> false
+// Number instanceof Function . . .	-> true
+// typeof 1	. . . . . . . . . . . .	-> "number"
+// typeof new Number(1) . . . . . .	-> "object"
+// typeof Number  . . . . . . . . .	-> "function"
 
-// new Number(0) instanceof Number  -> true
-// typeof new Number(0) ........... -> "object"
-
-// (()=>{}) instanceof Object ..... -> true
-// typeof (()=>{}) ................ -> "function"
-
-// checks for these inputs (vvv) will be through typeof to fix javascript type weirdness
+// checks for types below will be through typeof to fix javascript type weirdness
+// replaces 'instanceof' check with 'typeof' for these
 const primitives = [Object, String, Number, Boolean, BigInt, Symbol];
 
 /**
  * @param {{[varname: string]: any}} values Array/single variable name(s): { varname1, varname2 }
  * @param types Array/single class type(s) and/or literal value(s)
- * @param {boolean} invert Flips the assertion - asserts none of the values are of any of the defined type(s)
  */
-export default function assertType(values, types, invert = false) {
+export const isType = (values, types) => {
 	if (!values || typeof values !== "object" || values instanceof Array)
 		sendError({ values }, [Object]);
 	if (!types) sendError({ types }, [Array]);
-	if (typeof invert !== "boolean") sendError({ invert }, [Boolean]);
-
 	if (!Array.isArray(types)) types = [types];
+
 	const objectTypes = types.filter(e => e instanceof Function);
 	const literalTypes = types.filter(e => !(e instanceof Function));
 
@@ -76,6 +72,20 @@ export default function assertType(values, types, invert = false) {
 		const doesMatch =
 			objectTypesMatching.length !== 0 || literalTypes.includes(value);
 
-		if (!doesMatch ^ invert) sendError({ [name]: value }, types, invert);
+		if (!doesMatch) return false;
 	}
-}
+	return true;
+};
+
+/**
+ * @param {{[varname: string]: any}} values Array/single variable name(s): { varname1, varname2 }
+ * @param types Array/single class type(s) and/or literal value(s)
+ * @param {boolean} invert Flips the assertion - asserts none of the values are of any of the defined type(s)
+ */
+export const assertType = (values, types, invert = false) => {
+	if (typeof invert !== "boolean") sendError({ invert }, [Boolean]);
+	if (!isType(values, types) ^ invert) {
+		if (!Array.isArray(types)) types = [types];
+		sendError(values, types, invert);
+	}
+};
